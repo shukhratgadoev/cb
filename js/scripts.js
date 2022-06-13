5e84184d4e52ebcedbb7a37c1baa2ce7ee6aa886
Shukhrat Gadoev <shukhratgadoev@77projects.com>
2022-06-13 16:36:18 +0500

    feat: add one more block

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// function enterEvent(e) {
+//     let elems = document.querySelector(".table-items--white");
+//     console.log('enterEvent', elems)
+//     if (elems !== null) {
+//         elems.classList.remove("table-items--white");
+//     }
+
+// }onmousemove="enterEvent(event)" onmouseleave="leaveEvent(event)"
+
+// function leaveEvent(e) {
+//     let elems = document.querySelector(".table-items--white");
+//     console.log('leaveEvent', elems)
+//     if (elems === null) {
+//         elems.classList.add("table-items--white");
+//     }
+
+// }
